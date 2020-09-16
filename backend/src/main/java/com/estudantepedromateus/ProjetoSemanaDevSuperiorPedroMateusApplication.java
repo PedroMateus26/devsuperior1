3f771b45fcf90a6c1cdf069ad17f3b6cf0de758c
PedroMateus26 <pedromateusneto@gmail.com>
2020-09-16 08:09:16 -0300

    Project created

---
+++ backend/src/main/java/com/estudantepedromateus/ProjetoSemanaDevSuperiorPedroMateusApplication.java
@@ -0,0 +1,13 @@
+package com.estudantepedromateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSemanaDevSuperiorPedroMateusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSemanaDevSuperiorPedroMateusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/estudantepedromateus/ProjetoSemanaDevSuperiorPedroMateusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudantepedromateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSemanaDevSuperiorPedroMateusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
